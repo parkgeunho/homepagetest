9b9e2c24d41319ea669157f3148da50909a03701
GHpark <park@DESKTOP-763D91M>
2016-10-26 10:36:07 +0900

    ㅇㅅㅇ

--- homepage/src/main/java/com/homepage/portfolio/Controller/BoardController.java
+++ homepage/src/main/java/com/homepage/portfolio/Controller/BoardController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.log4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.homepage.portfolio.FileUpUtill;
-import com.homepage.portfolio.HomeController;
-import com.homepage.portfolio.PagingUtill;
 import com.homepage.portfolio.DTO.BoardDTO;
 import com.homepage.portfolio.DTO.FileDTO;
 import com.homepage.portfolio.DTO.ReplyDTO;
