c85ae54cb69787aa3cc4f5efc34bc26016d1a334
GHpark <park@DESKTOP-763D91M>
2016-11-03 17:27:22 +0900

    ㅇ

--- homepage/src/main/java/com/homepage/portfolio/HomeController.java
+++ homepage/src/main/java/com/homepage/portfolio/HomeController.java
@@ -2,19 +2,27 @@ package com.homepage.portfolio;
 
 import java.text.DateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.homepage.portfolio.DTO.BoardDTO;
-import com.homepage.portfolio.Service.BoardService;
+import com.homepage.GetMapping;
+
 
 /**
  * Handles requests for the application home page.
@@ -24,19 +32,34 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	OAuth2Operations oAuth2Operations;
 	
-	
-	
-	
-	
+	@Autowired
+	FacebookConnectionFactory connectionFactory;
 	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model,String code,HttpServletRequest req, HttpServletResponse res) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		String access = code;
+		
+		System.out.println(access);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -48,12 +71,20 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	@RequestMapping(value = "/facebook", method = RequestMethod.GET)
 	public String test(Locale locale, Model model) {
 		
-		return "test";
+		return "facebook";
 	}
 	
+	
+	
+	
+
+	
+	
+	
+	 
 	
 	
 	
