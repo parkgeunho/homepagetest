1ba8b6a30e303e39888c83dee9aae43b66bfd7dc
GHpark <park@DESKTOP-763D91M>
2016-11-03 17:35:32 +0900

    ㅁㄴㅇ

--- homepage/src/main/java/com/homepage/portfolio/HomeController.java
+++ homepage/src/main/java/com/homepage/portfolio/HomeController.java
@@ -21,7 +21,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.homepage.GetMapping;
 
 
 /**
